c9678dc9ee62aa6a4e48f13a077c1af18a721687
pennd44 <pdiba@oxbowschool.org>
2020-09-22 10:54:16 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+ for(let i=1; i<arr.length; i++){
+  let el = arr[i]
+  let j=i-1
+  for(j; j>=0 && arr[j]>el; j--){
+    arr[j+1]=arr[j]
+  }
+  arr[j+1]=el
+}
+return arr
+
+
+
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,19 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+ 
+for(let i=0; i<arr.length; i++){
+  let minimumIndex = i
+  for(let j=i+1; j<arr.length; j++){
+    if(arr[j]<arr[minimumIndex]){
+      minimumIndex=j
+    }}
+  if(minimumIndex != i){
+    let temporary = arr[i]
+    arr[i]=arr[minimumIndex]
+    arr[minimumIndex]=temporary
+  }}
+
+
+
 }
